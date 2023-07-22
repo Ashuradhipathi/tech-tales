23705c95898b03a23ea6ef30f8d794a630e22cee
sri <97357548+srivanik8@users.noreply.github.com>
2023-07-23 00:13:56 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -20,7 +20,7 @@ def main():
     st.markdown("<br>", unsafe_allow_html=True)
     st.markdown("<br>", unsafe_allow_html=True)
     st.markdown("<br>", unsafe_allow_html=True)
-     st.markdown("""
+    st.markdown("""
     <div style='text-align: center;'>
         <h5> Ever wondered about the person behind the code? With TechTales, you can now access personal information such as names, emails, and bios of GitHub users, shared willingly by them in the spirit of openness and connection.</h5>
     </div>
