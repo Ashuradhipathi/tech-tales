97f4ebdb5c390c0ae403ff78e10c3fc0fb059900
sri <97357548+srivanik8@users.noreply.github.com>
2023-07-23 00:40:24 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -16,8 +16,9 @@ def main():
         <h1>Tech-Tales</h1>
     </div>
     """, unsafe_allow_html=True)
-    st.markdown(""" <style>
+    st.markdown(""" <style> {
     background-image: url("https://i.ibb.co/FYgXtTZ/3d-rendering-emotions.jpg");
+    }
     </style> """, 
     unsafe_allow_html=True)
     st.markdown("<br>", unsafe_allow_html=True)
