d08bcd4df19e03ba100ca1b9fb1842b64d6668ef
sri <97357548+srivanik8@users.noreply.github.com>
2023-07-23 09:59:41 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -23,7 +23,9 @@ def main():
     st.markdown("<br>", unsafe_allow_html=True)
     st.markdown("""
     <div style='text-align: center;'>
-        <h5> Ever wondered about the person behind the code? With TechTales, you can now access personal information such as names, emails, and bios of GitHub users, shared willingly by them in the spirit of openness and connection.</h5>
+        <h4> Ever wondered about the person behind the code?</h4>
+        <h5>With TechTales, you can now access personal information such as names, emails, and bio of GitHub users, shared willingly by them in the spirit of openness and connection.</h5>
+        <br>
         <h5>TechTales enables anonymous commenting on GitHub profiles, allowing you to share your thoughts, feedback, and encouragement without revealing your identity. And here's the kicker - you can even opt-in to associate your phone number with a GitHub user, and receive real-time SMS notifications whenever someone leaves a comment on their profile</h5>
     </div>
     """, unsafe_allow_html=True)
